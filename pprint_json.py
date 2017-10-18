d28b152485242786ad5c37d1995be128702e0970
oursupreamacy <oursupremacy931@gmail.com>
2017-10-18 21:28:35 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -1,4 +1,3 @@
-# encoding: utf-8
 import json
 import sys
 
@@ -9,12 +8,11 @@ def load_data(filepath):
     return parsed
 
 
-def pretty_print_json(data):
-    print(json.dumps(data, indent=4, sort_keys=True))
+def pretty_print_json(parsed):
+    print(json.dumps(parsed, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
-    l = sys.argv[1]
-    r = load_data(l)
-    pretty_print_json(r)
+    filepath = sys.argv[1]
+    pretty_print_json(load_data(filepath))
 
